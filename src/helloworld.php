2a5f9dc07e0a4598495c34831ca94590e5da06db
2211082004 <athirarahmadini14@gmail.com>
2024-05-29 09:20:40 +0700

    commit pertama

---
+++ src/helloworld.php
@@ -0,0 +1,9 @@
+<?
+
+namespace athira\libhallo;
+
+class helloworld{
+    public function sayHello($name){
+        return "Hello $name welcome to php and composer to create ypur own library";
+    }
+}
